69c539792159335ab317ed9f369491eb22caafa0
Aaron Cohn <aaron.cohn@workday.com>
2023-12-17 14:01:21 -0800

    Improvements to Day 1

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.Character.isDigit;
+import org.jetbrains.annotations.NotNull;
 
 public class Day1 implements AoC23Day {
     private static final HashMap<String, Integer> wordToIntMap = buildWordToDigitMap();
@@ -18,7 +18,7 @@ public class Day1 implements AoC23Day {
     private static final Pattern digitsPattern = Pattern.compile("\\d");
     private final String filename;
 
-    private static HashMap<String, Integer> buildWordToDigitMap() {
+    private static @NotNull HashMap<String, Integer> buildWordToDigitMap() {
         HashMap<String, Integer> m = new HashMap<>();
         m.put("one", 1);
         m.put("two", 2);
@@ -44,28 +44,17 @@ public class Day1 implements AoC23Day {
 
     private void part1() {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            int sum = 0;
-            while ((line = br.readLine()) != null) {
-                int length = line.length();
-                int d1 = -1, d2 = -1;
-                for (int i = 0; i < length; i++) {
-                    if (isDigit(line.charAt(i))) {
-                        d1 = Character.getNumericValue(line.charAt(i));
-                        break;
-                    }
+            int sum = br.lines().mapToInt(line -> {
+                Matcher m = digitsPattern.matcher(line);
+                int d1 = m.find() ? getDigit(m.group()) : 0;
+                String last = null;
+                while (m.find()) {
+                    last = m.group();
                 }
-                for (int j = length - 1; j >= 0; j--) {
-                    if (isDigit(line.charAt(j))) {
-                        d2 = Character.getNumericValue(line.charAt(j));
-                        break;
-                    }
-                }
-                sum += (10 * d1) + d2;
-            }
+                int d2 = (last != null) ? getDigit(last) : d1;
+                return (10 * d1) + d2;
+            }).sum();
             System.out.println("The sum is:" + sum);
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find the file: day1.txt");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
@@ -73,40 +62,40 @@ public class Day1 implements AoC23Day {
 
     private void part2() {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String line;
-            int sum = 0;
-            while ((line = br.readLine()) != null) {
+            int sum = br.lines().mapToInt(line -> {
                 int d1, d2;
                 if (line.length() == 1) {
                     d1 = d2 = Character.getNumericValue(line.charAt(0));
                 } else {
-                    String first = null;
-                    Matcher m1 = digitsAndWordsPattern.matcher(line);
-                    if (m1.find()) {
-                        first = m1.group();
-                    }
-                    String last = null;
-                    for (int i = line.length() - 1; i >= 0; i--) {
-                        Matcher m2 = digitsAndWordsPattern.matcher(line.substring(i));
-                        if (m2.find()) {
-                            last = m2.group();
-                            break;
-                        }
-                    }
-                    if (last == null) {
-                        throw new Exception("File contents did not meet expectations :-(");
-                    }
-                    d1 = getDigit(first);
-                    d2 = getDigit(last);
+                    d1 = getFirstDigit(line);
+                    d2 = getLastDigit(line);
                 }
-                sum += (10 * d1) + d2;
-            }
+                return (10 * d1) + d2;
+            }).sum();
             System.out.println("The sum is:" + sum);
         } catch (FileNotFoundException e) {
             System.out.println("Couldn't find the file: day1.txt");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static int getFirstDigit(String line) {
+        Matcher m = digitsAndWordsPattern.matcher(line);
+        String first = m.find() ? m.group() : null;
+        return getDigit(first);
+    }
+
+    private static int getLastDigit(@NotNull String line) {
+        String last = null;
+        for (int i = line.length() - 1; i >= 0; i--) {
+            Matcher m2 = digitsAndWordsPattern.matcher(line.substring(i));
+            if (m2.find()) {
+                last = m2.group();
+                break;
+            }
+        }
+        return getDigit(last);
     }
 
     private static int getDigit(String s) {

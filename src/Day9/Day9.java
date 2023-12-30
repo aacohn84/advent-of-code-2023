218aae5580e34454d430aea89c17284f7cbefbc2
Aaron Cohn <aaron.cohn@workday.com>
2023-12-29 21:02:44 -0800

    Day 9 part 2 completed

--- src/Day9/Day9.java
+++ src/Day9/Day9.java
@@ -53,7 +53,40 @@ public class Day9 extends AoC23Day {
     }
 
     @Override
-    protected void part2(BufferedReader br) throws IOException {
-
+    protected void part2(@NotNull BufferedReader br) throws IOException {
+        int sum = 0;
+        String line;
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            // each line represents a sequence. for each sequence, we'll find the next value and add it to the sum.
+            // we start by recording the sequence into a list
+            String[] values = line.split("\\s+");
+            List<Integer> sequence = new ArrayList<>();
+            for (String value : values) {
+                sequence.add(parseVal(value));
+            }
+            // for each PAIR of values, we need to get the difference and store that in a sub-sequence.
+            // if that sequence is all zeroes, then we can move on to the next step, otherwise we have to create another
+            // subsequence
+            List<Integer> firsts = new ArrayList<>(); // the last value of each sequence
+            firsts.add(sequence.get(0)); // add the last value of the main sequence
+            List<Integer> subsequence;
+            do {
+                subsequence = new ArrayList<>();
+                int seqLen = sequence.size();
+                for (int i = 0, j = 1; j < seqLen; i++, j++) {
+                    int iVal = sequence.get(i);
+                    int jVal = sequence.get(j);
+                    subsequence.add(jVal - iVal);
+                }
+                firsts.add(subsequence.get(0));
+                sequence = new ArrayList<>(subsequence);
+            } while (!subsequence.stream().allMatch(x -> x == 0));
+            int seqNext = 0;
+            for (int i = firsts.size() - 2; i >= 0; i--) {
+                seqNext = firsts.get(i) - seqNext;
+            }
+            sum += seqNext;
+        }
+        System.out.println("Part 2 sum: " + sum);
     }
 }
